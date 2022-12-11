11a4acab0d3a438356715ab9f188a88f77474e7c
arlosham <52860219+mdshamim121@users.noreply.github.com>
2022-12-11 17:54:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slideToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    });
+});
